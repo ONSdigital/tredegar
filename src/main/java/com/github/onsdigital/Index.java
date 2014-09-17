343677409ecdbcb7c59bb23c67bbbfd87558e89e
David Carboni <david@carboni.io>
2014-09-17 13:33:46 +0100

    Index redirect and placeholder index.html

---
+++ src/main/java/com/github/onsdigital/Index.java
@@ -0,0 +1,11 @@
+package com.github.onsdigital;
+
+import com.github.davidcarboni.restolino.helpers.HomeRedirect;
+
+public class Index extends HomeRedirect {
+
+	public Index() {
+		super("/index.html");
+	}
+
+}
