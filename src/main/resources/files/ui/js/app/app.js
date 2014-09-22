356e157e05cee9ac050d544e413b99ba56998a18
Pastor <pastor.methods@gmail.com>
2014-09-22 17:32:08 +0100

    Added templates t1 t2 t3

--- src/main/resources/files/ui/js/app/app.js
+++ src/main/resources/files/ui/js/app/app.js
@@ -2,6 +2,41 @@ $(document).ready(function() {
   $('body').addClass('js');
 
   $(document).ready(function() {
-    $('.tooltip').tooltipster();
+    $('.tooltip').tooltipster({
+      'maxWidth': 270
+    });
   });
+
+  var placeHolderConfig = {
+    className: 'placeholder-polyfill'
+  };
+  Modernizr.load({
+    test: Modernizr.input.placeholder,
+    nope: [
+            '/ui/css/lib/placeholder_polyfill.min.css',
+            '/ui/js/lib/jquery.placeholder_polyfill.combo.min.js'
+          ]
+  });
+
+
+
+
+// create an observer instance
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      console.log(mutation);
+    });
+  });
+
+// configuration of the observer:
+  var config = { attributes: true, childList: true, characterData: true };
+
+  $('.icon--inline').each(function() {
+
+    observer.observe($(this)[0], config);
+  });
+
+// pass in the target node, as well as the observer options
+
+
 });
