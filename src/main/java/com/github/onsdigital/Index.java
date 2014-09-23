20c10059cd038ecdeea9103aba6e0d5d30a706c1
David Carboni <david@carboni.io>
2014-09-23 11:27:29 +0100

    Fix for blank home page. Two things: 1) home page is now t1.html 2) Restolino wasn't seeing Index as an implementation of the Home interface, possibly because HomeRedirect is excluded by the package prefix configuration.

--- src/main/java/com/github/onsdigital/Index.java
+++ src/main/java/com/github/onsdigital/Index.java
@@ -1,11 +1,12 @@
 package com.github.onsdigital;
 
+import com.github.davidcarboni.restolino.framework.Home;
 import com.github.davidcarboni.restolino.helpers.HomeRedirect;
 
-public class Index extends HomeRedirect {
+public class Index extends HomeRedirect implements Home {
 
 	public Index() {
-		super("/index.html");
+		super("/t1.html");
 	}
 
 }
