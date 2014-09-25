6631f9b902a303a43d24ec628868949837d7d3cf
David Carboni <david@carboni.io>
2014-09-25 14:32:08 +0100

    Fixed java command line.

--- generate.bat
+++ generate.bat
@@ -8,4 +8,4 @@ echo What's this in Windows Latin?
 echo rm -rf src/main/taxonomy
 
 REM # Generate the new output:
-java -cp "target/classes:target/dependency/*.jar" com.github.onsdigital.generator.Csv
+java -cp "target/classes:target/dependency/*" com.github.onsdigital.generator.Csv
