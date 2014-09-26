e5fa016dabe3d440a37269657af8a4c24712357e
Aaron Witter <aaronwitter@hotmail.com>
2014-09-26 14:18:44 +0100

    updated run.bat which allows restolino to run on windows platform

--- run.bat
+++ run.bat
@@ -9,13 +9,13 @@ set RESTOLINO_CLASSES="target/classes"
 REM # Elasticsearch
 set BONSAI_URL=http://localhost:9200
 
-REM # Build ^
-mvn clean package && ^
- ^
-REM # Generate taxonomy ^
-java -cp "target/classes:target/dependency/*" com.github.onsdigital.generator.Csv && ^
- ^
-REM # Development: reloadable ^
+REM # Build 
+mvn clean package -DskipTests && ^
+java %JAVA_OPTS% -Drestolino.files=%RESTOLINO_STATIC% -Drestolino.classes=%RESTOLINO_CLASSES% -cp "target/dependency/*" com.github.davidcarboni.restolino.Main 
+REM # Generate taxonomy 
+REM java -cp "target/classes:target/dependency/*" com.github.onsdigital.generator.Csv && 
+ 
+REM # Development: reloadable 
 java %JAVA_OPTS% -Drestolino.files=%RESTOLINO_STATIC% -Drestolino.classes=%RESTOLINO_CLASSES% -cp "target/dependency/*" com.github.davidcarboni.restolino.Main
 
 REM # Production: non-reloadable
