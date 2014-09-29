f77fa47a3bf4cd2598de59e9448356979e6501e4
David Carboni <david@carboni.io>
2014-09-29 17:28:52 +0100

    Decided not to delete the existing ui directory when copying updates across.

--- cx.sh
+++ cx.sh
@@ -7,7 +7,7 @@ grunt build &&
 cd $PROJECT_DIR &&
 
 # Replace old files with new files:
-rm -rf src/main/resources/files/ui &&
+#rm -rf src/main/resources/files/ui &&
 cp -r src/main/scss/ui src/main/resources/files/ &&
 rm src/main/resources/files/ui/config.json &&
 rm -rf src/main/resources/files/ui/scss
