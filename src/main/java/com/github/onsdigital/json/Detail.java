ce1fd87ffe9ad87f51d4bdef4aca53ac5076f6ac
David Carboni <david@carboni.io>
2014-09-30 17:25:50 +0100

    Java objects to deal with additional data.json content.

---
+++ src/main/java/com/github/onsdigital/json/Detail.java
@@ -0,0 +1,18 @@
+package com.github.onsdigital.json;
+
+public class Detail {
+
+	public String name;
+	public String info = "Lorem ipsum dolor sit amet";
+	public String number;
+	public String unit;
+	public String date;
+
+	public Detail(String name, String number, String unit, String date) {
+		this.name = name;
+		this.number = number;
+		this.unit = unit;
+		this.date = date;
+
+	}
+}
