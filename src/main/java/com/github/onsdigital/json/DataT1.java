6ea3ea027bcd0655388d7a3a3a6086c329b6f5ad
David Carboni <david@carboni.io>
2014-10-01 13:54:29 +0100

    Default data for each of t1, t2 and t3

---
+++ src/main/java/com/github/onsdigital/json/DataT1.java
@@ -0,0 +1,12 @@
+package com.github.onsdigital.json;
+
+import com.github.onsdigital.generator.Folder;
+
+public class DataT1 extends Data {
+
+	public DataT1(Folder folder) {
+		super(folder);
+		level = "t1";
+	}
+
+}
