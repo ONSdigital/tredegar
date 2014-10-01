a157998609f731c8076a29b337448ec9b041c75b
David Carboni <david@carboni.io>
2014-10-01 15:31:49 +0100

    Reduced the code to a couple of loops.

--- src/main/resources/files/ui/js/t1.js
+++ src/main/resources/files/ui/js/t1.js
@@ -65,51 +65,35 @@ $( document ).ready(function() {
 
 	// Breadcrumb
 	var breadcrumb = $(".breadcrumb")
-	var breadcrumbItem = $("li:eq(0)", section1)
+	var breadcrumbItem = $("li:eq(0)", breadcrumb)
 	breadcrumbItem.detach()
 	$("li", breadcrumb).remove()
 
-	// Section blocks
-	var section1 = $(".nav-panel--stats:eq(0)")
-	var section2 = $(".nav-panel--stats:eq(1)")
-	var section3 = $(".nav-panel--stats:eq(2)")
-	var section4 = $(".nav-panel--stats:eq(3)")
+	var section = new Array();
+	var header = new Array();
+	var sectionItem = new Array();
+	var footer = new Array();
 
-	// Section headers - set placeholders:
-	var header1 = $("header", section1)
-	var header2 = $("header", section2)
-	var header3 = $("header", section3)
-	var header4 = $("header", section4)
-	$("h2", header1).text("Loading..")
-	$("h2", header2).text("Loading..")
-	$("h2", header3).text("Loading..")
-	$("h2", header4).text("Loading..")
+	for (var i = 0; i < 4; i++) {
 
-	// Section items
-	// - detach one to use as a template and remove the rest:
-	var section1Item = $("li:eq(0)", section1)
-	var section2Item = $("li:eq(0)", section2)
-	var section3Item = $("li:eq(0)", section3)
-	var section4Item = $("li:eq(0)", section4)
-	section1Item.detach()
-	section2Item.detach()
-	section3Item.detach()
-	section4Item.detach()
-	$("li", section1).remove()
-	$("li", section2).remove()
-	$("li", section3).remove()
-	$("li", section4).remove()
+		// Section blocks
+		section.push($(".nav-panel--stats:eq("+i+")"))
 
-	// Section footers
-	// - detach these to use as a template:
-	var footer1 = $("footer", section1)
-	var footer2 = $("footer", section2)
-	var footer3 = $("footer", section3)
-	var footer4 = $("footer", section4)
-	footer1.detach()
-	footer2.detach()
-	footer3.detach()
-	footer4.detach()
+		// Section headers - set placeholders:
+		header.push($("header", section[i]))
+		$("h2", header[i]).text("Loading..")
+
+		// Section items
+		// - detach one to use as a template and remove the rest:
+		sectionItem.push($("li:eq(0)", section[i]))
+		sectionItem[i].detach()
+		$("li", section[i]).remove()
+
+		// Section footers
+		// - detach these to use as a template:
+		footer.push($("footer", section[i]))
+		footer[i].detach()
+	}
 
 	/* Get the data.json file to populate the page: */
 
@@ -122,52 +106,19 @@ $( document ).ready(function() {
 		$("p", ".lede").text(data.lede);
 		$(".content-reveal__hidden").text(data.more)
 
-		// Section 1
-		if (data.children.length > 0) {
-			var item = data.children.shift()
-			$("h2", header1).text(item.name)
-			while (item.detail.length > 0) {
-				var detailItem = populateDetail(item.detail.shift(), section1Item)
-				$("ul", section1).append(detailItem)
-			}
-			$("a", footer1).text("View all " + item.name).attr("href", link(item.fileName))
-			section1.append(footer1)
-		}
+		for (var i = 0; i < 4; i++) {
 
-		// Section 2
-		if (data.children.length > 0) {
-			var item = data.children.shift()
-			$("h2", header2).text(item.name)
-			while (item.detail.length > 0) {
-				var detailItem = populateDetail(item.detail.shift(), section2Item)
-				$("ul", section2).append(detailItem)
+			// Sections
+			if (data.children.length > 0) {
+				var item = data.children.shift()
+				$("h2", header[i]).text(item.name)
+				while (item.detail.length > 0) {
+					var detailItem = populateDetail(item.detail.shift(), sectionItem[i])
+					$("ul", section[i]).append(detailItem)
+				}
+				$("a", footer[i]).text("View all " + item.name).attr("href", link(item.fileName))
+				section[i].append(footer[i])
 			}
-			$("a", footer2).text("View all " + item.name).attr("href", link(item.fileName))
-			section2.append(footer2)
-		}
-
-		// Section 3
-		if (data.children.length > 0) {
-			var item = data.children.shift()
-			$("h2", header3).text(item.name)
-			while (item.detail.length > 0) {
-				var detailItem = populateDetail(item.detail.shift(), section3Item)
-				$("ul", section3).append(detailItem)
-			}
-			$("a", footer3).text("View all " + item.name).attr("href", link(item.fileName))
-			section3.append(footer3)
-		}
-
-		// Section 4
-		if (data.children.length > 0) {
-			var item = data.children.shift()
-			$("h2", header4).text(item.name)
-			while (item.detail.length > 0) {
-				var detailItem = populateDetail(item.detail.shift(), section4Item)
-				$("ul", section4).append(detailItem)
-			}
-			$("a", footer4).text("View all " + item.name).attr("href", link(item.fileName))
-			section4.append(footer3)
 		}
 
 		// Breadcrumb
