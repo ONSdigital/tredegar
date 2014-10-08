896cad7e66853b7b53e53ee7b353a7ec0ddde8ed
Pastor <pastor.methods@gmail.com>
2014-10-08 15:46:42 +0100

    Changes to methodology page

--- src/main/resources/files/ui/js/methodology.js
+++ src/main/resources/files/ui/js/methodology.js
@@ -165,7 +165,7 @@ $( document ).ready(function() {
 
     /* Get the data.json file to populate the page: */
 
-    $.get( "http://localhost:8080/bulletin.json", function( data1 ) {
+    $.get( "http://localhost:8080/methodology.json", function( data1 ) {
 
         // Titles:
         setTitle(data1.title)
