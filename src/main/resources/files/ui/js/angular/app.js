3957ca92d5638b90af1eb464df9ec739237dbdc3
Bren <brhneser@gmail.com>
2014-10-12 21:01:31 +0100

    Angular t1 and t2 completed

--- src/main/resources/files/ui/js/angular/app.js
+++ src/main/resources/files/ui/js/angular/app.js
@@ -12,13 +12,34 @@ var onsApp = angular.module('onsApp', [
 onsApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-     when('/',{
-        redirectTo: '/home'
-      }).
-     otherwise({
-        templateUrl: 'templates/template.html'
-      });
-  }]);
+    when('/', {
+      redirectTo: '/home'
+    }).
+    otherwise({
+      templateUrl: 'templates/template.html'
+    });
+  }
+]);
+
+onsApp.filter('slice', function() {
+  return function(arr, start, end) {
+      return arr.slice(start, end);
+  };
+});
+
+
+
+onsApp.factory('Page', function() {
+  var title = 'default';
+  return {
+    title: function() {
+      return title;
+    },
+    setTitle: function(newTitle) {
+      title = newTitle
+    }
+  };
+});
 
 
 // $(document).ready(function() {
