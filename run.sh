d064a809678c29d1b2e65d3c4ff98e482a151ad1
David Carboni <david@carboni.io>
2014-10-14 16:28:13 +0100

    updates to test HTTP basic auth.

--- run.sh
+++ run.sh
@@ -7,6 +7,11 @@ export RESTOLINO_STATIC="src/main/resources/files"
 export RESTOLINO_CLASSES="target/classes"
 export PACKAGE_PREFIX=com.github.onsdigital
 
+# For testing out HTTP basic auth
+#export USERNAME=stats
+#export PASSWORD=titchfield
+#export REALM=onsalpha
+
 # Elasticsearch
 export BONSAI_URL=http://localhost:9200
 export BONSAI_CLUSTERNAME=elasticsearch
@@ -26,7 +31,7 @@ java -cp "target/classes:target/dependency/*" com.github.onsdigital.generator.Cs
 mvn package && \
 
 # Development: reloadable
-java $JAVA_OPTS -Drestolino.files=$RESTOLINO_STATIC -Drestolino.classes=$RESTOLINO_CLASSES -Drestolino.packageprefix=$PACKAGE_PREFIX -cp "target/dependency/*" com.github.davidcarboni.restolino.Main
+java $JAVA_OPTS -Drestolino.username=$USERNAME -Drestolino.password=$PASSWORD -Drestolino.realm=$REALM -Drestolino.files=$RESTOLINO_STATIC -Drestolino.classes=$RESTOLINO_CLASSES -Drestolino.packageprefix=$PACKAGE_PREFIX -cp "target/dependency/*" com.github.davidcarboni.restolino.Main
 
 # Production: non-reloadable
 #java $JAVA_OPTS -jar target/*-jar-with-dependencies.jar
