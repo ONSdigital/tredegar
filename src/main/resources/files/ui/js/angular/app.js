5167126bac9167f55f245d4e0f18ed641c7328f6
Pastor <pastor.methods@gmail.com>
2014-10-16 16:58:30 +0100

    Angular Accordion

--- src/main/resources/files/ui/js/angular/app.js
+++ src/main/resources/files/ui/js/angular/app.js
@@ -221,3 +221,48 @@ onsApp.directive('markdown', function($http) {
             };
 
         });
+
+
+function AccordionCtrl($scope) {
+    //initiate an array to hold all active tabs
+    $scope.activeTabs = [];
+
+    //------ If you want to have only one opened at a time ------
+    //
+    // $scope.openTab = function (tab) {
+    //     $scope.activeTabs.splice($scope.activeTabs.indexOf(tab), 1);
+    //     //check if tab is already open
+    //     if ($scope.isOpenTab(tab)) {
+    //         //if it is, remove it from the activeTabs array
+    //         $scope.activeTabs.splice($scope.activeTabs.indexOf(tab), 1);
+    //     } else {
+    //         //if it's not, add it!
+    //         $scope.activeTabs.push(tab);
+    //     }
+    // }
+    //
+
+    // check if the tab is active
+    $scope.isOpenTab = function (tab) {
+        //check if this tab is already in the activeTabs array
+        if ($scope.activeTabs.indexOf(tab) > -1) {
+            //if so, return true
+            return true;
+        } else {
+            //if not, return false
+            return false;
+        }
+    }
+
+    //function to 'open' a tab
+    $scope.openTab = function (tab) {
+        //check if tab is already open
+        if ($scope.isOpenTab(tab)) {
+            //if it is, remove it from the activeTabs array
+            $scope.activeTabs.splice($scope.activeTabs.indexOf(tab), 1);
+        } else {
+            //if it's not, add it!
+            $scope.activeTabs.push(tab);
+        }
+    }
+}
