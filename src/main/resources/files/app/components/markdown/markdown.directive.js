a082081a50912b3f74525ec278ec68b5a60aa885
Bren <brhneser@gmail.com>
2014-10-23 23:16:25 +0100

    Several small fixes

---
+++ src/main/resources/files/app/components/markdown/markdown.directive.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('onsComponents')
+.directive('markdown', function($http) {
+    var converter = new Showdown.converter();
+    return {
+        restrict: 'A',
+        scope: {
+            link: '@'
+        },
+        template: '<p></p>',
+        link: function(scope, element, attrs) {
+            attrs.$observe('link', function(link) {
+                var htmlText = converter.makeHtml(link);
+                element
+                    .html(htmlText)
+                    .find("pre")
+                    .replaceWith("<p>"+
+                '<img src="/ui/img/equation.png" alt="sample chart">'+
+            "</p>");
+                element
+                    .find("blockquote").find("p")
+                    .addClass("box--padded--highlight");
+                element
+                    .find("ul").addClass("list--neutral box--cyan--separated-left")
+                    .find("ul").find("li").addClass("big");
+            });
+        }
+    };
+});
