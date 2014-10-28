8b865606811495a87dd6b90bf6c39de564a6f860
daviesajw <andrewjwdavies@gmail.com>
2014-10-28 08:23:36 +0000

    fixing collection link from economy t2 level

--- src/main/resources/files/app/app.js
+++ src/main/resources/files/app/app.js
@@ -43,6 +43,10 @@
                     templateUrl: 'app/templates/bulletin/bulletin.html',
                     controller: 'BulletinCtrl'
                 }).
+                when('/economy/collection', {
+                    templateUrl: 'app/templates/collection/collection.html',
+                    controller: "CollectionCtrl"
+                }).
                 when('/economy/inflationandpriceindices/collection', {
                     templateUrl: 'app/templates/collection/collection.html',
                     controller: "CollectionCtrl"
