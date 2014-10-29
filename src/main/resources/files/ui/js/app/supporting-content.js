bc2032d3b6914849bc6433c66a4dc9675b1b8299
Bren <brhneser@gmail.com>
2014-10-29 16:44:37 +0000

    Deleted old files from unstructured Angular app

--- src/main/resources/files/ui/js/app/supporting-content.js
+++
@@ -1,5 +0,0 @@
-$(document).on('ready', function() {
-  $('.supporting-content__title').on('click', function() {
-    $(this).parent('.supporting-content').toggleClass('is-expanded is-collapsed');
-  });
-});
