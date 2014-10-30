2d1db12fa650810bfdcda87207df27a93cbe4edc
David Carboni <david@carboni.io>
2014-10-30 16:08:45 +0000

    Removed default values from timeseries. This doesn't seem to have affected the taxonomy. Suspicious. I can't believe that everything already had a value.

--- src/main/java/com/github/onsdigital/json/timeseries/TimeSeries.java
+++ src/main/java/com/github/onsdigital/json/timeseries/TimeSeries.java
@@ -11,26 +11,25 @@ import com.github.onsdigital.json.DataItem;
 public class TimeSeries extends DataItem {
 
 	// Spreadsheet headings
-	private String cdid = "aaaa";
-	public String seasonalAdjustment = "Not seasonally adjusted";
-	public String units = "Weights (parts per 1000)";
-	public String mainMeasure = "Consumer Prices Index";
-	public String description = "Consumer price indices are important indicators of how the UK economy is performing.\n"
-			+ "The indices are used in many ways by the government, businesses, and society in general. They can affect interest rates, tax allowances, wages, state benefits, pensions, maintenance, contracts and many other payments. They also show the impact of inflation on family budgets and affect the value of the pound in your pocket.";
-	public String note1 = "In accordance with the Statistics and Registration Service Act 2007, the Retail Prices Index and its derivatives have been assessed against the Code of Practice for Official Statistics and found not to meet the required standards for designation as National Statistics.";
-	public String note2 = "Changes in the internal purchasing power of a currency may be defined as  the 'inverse' of changes in the levels of prices; when prices go up, the  amount which can be purchased with a given sum of money goes down. The  monthly figures of the all items RPI can be used to obtain estimates of the  changes in prices or in purchasing power between any 2 months. To find the  purchasing power of the pound in one month, given that it was 100p in a  previous month the calculation is:  100p multiplied by the earlier month RPI then divided by the later month RPI.";
+	private String cdid;
+	public String seasonalAdjustment;
+	public String units;
+	public String mainMeasure;
+	public String description;
+	public String note1;
+	public String note2;
 
 	// Additional information
-	public String number = "126.7";
-	public String unit = "m";
+	public String number;
+	public String unit;
 	// A unit that needs to go before the number, e.g. £
 	public String preUnit;
-	public String date = "January 2014";
-	public String lastUpated = "16 Sep 2014";
-	public String nextUpdate = "14 Oct 2014";
+	public String date;
+	public String lastUpated;
+	public String nextUpdate;
 
 	// e.g. "Not a national statistic" or "2005 = 100. Not seasonally adjusted"
-	public String note = "2005 = 100. Not seasonally adjusted";
+	public String note;
 
 	// The nectar, the goodness, the very juice of the fireflower: data.
 	public List<TimeSeriesValue> data;
