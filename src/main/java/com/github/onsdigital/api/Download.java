029eeca04d2c04633ff762eb56d2e20edd448a0c
Bren <brhneser@gmail.com>
2014-10-31 08:37:49 +0000

    Partial download beckend functionality

--- src/main/java/com/github/onsdigital/api/Download.java
+++ src/main/java/com/github/onsdigital/api/Download.java
@@ -2,6 +2,9 @@ package com.github.onsdigital.api;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +12,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.core.Context;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.eclipse.jetty.http.HttpStatus;
 
 /**
  * 
@@ -16,16 +21,41 @@ import org.apache.commons.io.IOUtils;
  * 
  */
 import com.github.davidcarboni.restolino.framework.Endpoint;
+import com.github.davidcarboni.restolino.json.Serialiser;
+import com.github.onsdigital.api.taxonomy.Data;
+import com.github.onsdigital.bean.DownloadRequest;
+import com.github.onsdigital.json.timeseries.Timeseries;
 
 @Endpoint
 public class Download {
 
 	@POST
-	public void get(@Context HttpServletRequest httpServletRequest, @Context HttpServletResponse httpServletResponse) throws IOException {
-		httpServletResponse.setCharacterEncoding("UTF8");
-		httpServletResponse.setHeader("Content-Disposition", "attachment; filename=data.xls");
-		IOUtils.copy(new StringReader("Downloaded content"), httpServletResponse.getOutputStream());
+	public void get(@Context HttpServletRequest request, @Context HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("UTF8");
+		response.setHeader("Content-Disposition", "attachment; filename=data.xls");
+		try {
+			IOUtils.copy(new StringReader("Downloaded content"), response.getOutputStream());
+			DownloadRequest downloadRequest = Serialiser.deserialise(request, DownloadRequest.class);
+			processRequest(downloadRequest);
+		} catch (IOException e) {
+			response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR_500);
+			response.setContentType("text/plain");
+			response.getWriter().write("An error occured while processing download request");
+		}
+	}
 
+	private void processRequest(DownloadRequest downloadRequest) throws IOException {
+		List<Timeseries> dataList = new ArrayList<Timeseries>();
+		for (String uri : downloadRequest.urlList) {
+			dataList.add(Serialiser.deserialise(Files.newInputStream(Data.getData(uri)), Timeseries.class));
+		}
+		generateFiles(dataList);
+	}
+
+	private void generateFiles(List<Timeseries> dataList) {
+		for (Timeseries timeseries : dataList) {
+			System.out.println(ToStringBuilder.reflectionToString(timeseries.name));
+		}
 	}
 
 }
---
+++ src/main/java/com/github/onsdigital/bean/DownloadRequest.java
@@ -0,0 +1,10 @@
+package com.github.onsdigital.bean;
+
+import java.util.List;
+
+public class DownloadRequest {
+	
+	public String type;
+	public List<String> urlList;
+	
+}
