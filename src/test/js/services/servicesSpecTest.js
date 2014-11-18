167d0176d2699a56cf92a13c26f83d9429bdea60
daviesajw <andrewjwdavies@gmail.com>
2014-11-18 17:37:45 +0000

    reintroduce array tests

--- src/test/js/services/servicesSpecTest.js
+++ src/test/js/services/servicesSpecTest.js
@@ -1,19 +1,19 @@
 'use strict';
 
+// checks that all services are still available
 describe('services', function() {
 
   // load modules
   beforeEach(function() {
 	  angular.mock.module('onsApp')
+	  angular.mock.module('onsUtils')
   })
   
-  // Test service availability
   it('check the existence of Chart service', inject(function(Chart) {
       expect(Chart).toBeDefined();
     }));
   
-  // Test service availability
-//  it('check the existence of ArrayUtil service', inject(function(ArrayUtil) {
-//      expect(Chart).toBeDefined();
-//    }));
+  it('check the existence of ArrayUtil service', inject(function(ArrayUtil) {
+      expect(ArrayUtil).toBeDefined();
+    }));
 });
