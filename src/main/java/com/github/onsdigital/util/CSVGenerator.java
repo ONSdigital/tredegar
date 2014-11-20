8db63531c71b8b960735cd4481d27662d0520b5d
David Carboni <david@carboni.io>
2014-11-20 16:57:54 +0000

    Added extra header information to CSV and XLS downloads.

--- src/main/java/com/github/onsdigital/util/CSVGenerator.java
+++ src/main/java/com/github/onsdigital/util/CSVGenerator.java
@@ -42,10 +42,13 @@ public class CSVGenerator {
 	private void generateCsvRows(CSVWriter writer) {
 		String[] row;
 
+		// Spacer row
+		writer.writeNext(new String[2]);
+
 		for (Entry<String, TimeseriesValue[]> rowData : data.entrySet()) {
 			int size = rowData.getValue().length + 1;
 			int i = 0;
-			row = newRow(size);
+			row = new String[size];
 			row[i++] = rowData.getKey();
 			for (TimeseriesValue timeseriesValue : rowData.getValue()) {
 				row[i++] = timeseriesValue.value;
@@ -54,25 +57,59 @@ public class CSVGenerator {
 		}
 	}
 
-	private void generateCsvHeaders(CSVWriter writer) {
+	private int generateCsvHeaders(CSVWriter writer) {
 
 		int size = timeseries.size() + 1;
-		String[] names = newRow(size);
-		String[] cdids = newRow(size);
 
-		int i = 0;
-		names[i] = "";
-		cdids[i++] = "Date";
+		// Rows
+		int row = 9;
+		String[] name = new String[size];
+		String[] cdid = new String[size];
+		String[] preUnit = new String[size];
+		String[] unit = new String[size];
+		String[] keyNote = new String[size];
+		String[] additionalText = new String[size];
+		String[] source = new String[size];
+		String[] note1 = new String[size];
+		String[] note2 = new String[size];
+
+		// Labels
+		int column = 0;
+		name[column] = "Name";
+		cdid[column] = "CDID";
+		preUnit[column] = "Pre unit";
+		unit[column] = "Units";
+		keyNote[column] = "Key note";
+		additionalText[column] = "Date";
+		source[column] = "Source";
+		note1[column] = "Note 1";
+		note2[column] = "Note 2";
+		column++;
+
+		// Data
 		for (Timeseries timeseries : this.timeseries) {
-			names[i] = timeseries.name;
-			cdids[i++] = timeseries.cdid();
+			name[column] = timeseries.name;
+			cdid[column] = timeseries.cdid();
+			preUnit[column] = timeseries.preUnit;
+			unit[column] = timeseries.unit;
+			keyNote[column] = timeseries.keyNote;
+			additionalText[column] = timeseries.additionalText;
+			source[column] = timeseries.source;
+			note1[column] = timeseries.note1;
+			note2[column] = timeseries.note2;
+			column++;
 			System.out.println("Geneararing CSV for: " + timeseries.name + " at: " + timeseries.uri);
 		}
-		writer.writeNext(names);
-		writer.writeNext(cdids);
-	}
+		writer.writeNext(name);
+		writer.writeNext(cdid);
+		writer.writeNext(preUnit);
+		writer.writeNext(unit);
+		writer.writeNext(keyNote);
+		writer.writeNext(additionalText);
+		writer.writeNext(source);
+		writer.writeNext(note1);
+		writer.writeNext(note2);
 
-	private String[] newRow(int size) {
-		return new String[size];
+		return row;
 	}
 }
