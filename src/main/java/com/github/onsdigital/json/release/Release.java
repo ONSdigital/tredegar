680a613f33f7114d19595e808274c299540c9856
David Carboni <david@carboni.io>
2014-12-04 10:12:54 +0000

    Fixed up the Release class to clear compile errors.

--- src/main/java/com/github/onsdigital/json/release/Release.java
+++ src/main/java/com/github/onsdigital/json/release/Release.java
@@ -10,7 +10,6 @@ import com.github.onsdigital.json.dataset.Dataset;
 import com.github.onsdigital.json.markdown.Article;
 import com.github.onsdigital.json.markdown.Bulletin;
 import com.github.onsdigital.json.partial.Contact;
-import com.github.onsdigital.json.taxonomy.DataItemLink;
 import com.github.onsdigital.json.taxonomy.T3;
 
 /**
@@ -30,9 +29,9 @@ public class Release extends DataItem {
 			+ "be split into three distinct periods: 2000 to 2005, 2005 to 2012 and 2012 to June 2014.  Both indices experience greater variability within the second period of 2005 to 2012 than compared with the first and third period. From 2005 to 2012 the average growth rate was 2.9% for output price inflation and 8.1% for input price inflation.  The largest peaks and troughs experienced for both indices throughout the time series shown below occurred in the second period of 2005 to 2012. Output price inflation rose to 8.9% in July 2008 while input price inflation rose to 34.8% in June 2008 and both fell to their lowest rates in July 2009: output price inflation falling by 1.6% and input price inflation falling by 14.8%.";
 
 	// Sections
-	public List<DataItemLink> articles = new ArrayList<>();
-	public List<DataItemLink> bulletins = new ArrayList<>();
-	public List<DataItemLink> datasets = new ArrayList<>();
+	public List<ReleaseSection> articles = new ArrayList<>();
+	public List<ReleaseSection> bulletins = new ArrayList<>();
+	public List<ReleaseSection> datasets = new ArrayList<>();
 
 	public Release(T3 t3, Folder folder) {
 
@@ -43,14 +42,14 @@ public class Release extends DataItem {
 		fileName = t3.fileName;
 
 		for (Article article : folder.articles) {
-			articles.add(new DataItemLink(article.name, article.uri));
+			articles.add(new ReleaseSection(article));
 		}
 		for (Bulletin bulletin : folder.bulletins) {
-			bulletins.add(new DataItemLink(bulletin.name, bulletin.uri));
+			bulletins.add(new ReleaseSection(bulletin));
 		}
 		if (folder.datasets != null) {
 			for (Dataset dataset : folder.datasets) {
-				datasets.add(new DataItemLink(dataset.name, dataset.uri));
+				datasets.add(new ReleaseSection(dataset));
 			}
 		}
 	}
