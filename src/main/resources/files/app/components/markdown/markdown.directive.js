ea2581777d48f53056d5fb18f185b389de54ecb8
CarlHembrough <carlhuk@gmail.com>
2015-05-12 15:51:10 +0100

    Parsing custom markdown tag for charts.

--- src/main/resources/files/app/components/markdown/markdown.directive.js
+++ src/main/resources/files/app/components/markdown/markdown.directive.js
@@ -14,6 +14,15 @@ angular.module('onsComponents')
         link: function(scope, element, attrs) {
             attrs.$observe('link', function(link) {
                 var htmlText = converter.makeHtml(link);
+
+                    htmlText = htmlText.replace(/(<ons-chart\spath="[-A-Za-z0-9+&@#\/%?=~_|!:,.;\(\)*[\]$]+"?\s?\/>)/ig, function(match) {
+                        var path = $(match).attr('path');
+                        var output = '<div class="chart-container"><iframe frameBorder ="0" scrolling = "no" src="http://localhost:8081/florence/chart.html?path=' + path + '.json"></iframe></div>';
+                        //console.log(output);
+                        return output; //'[chart path="' + path + '" ]';
+                    });
+
+
                 element
                     .html(htmlText);
             //         .find("pre")
@@ -23,6 +32,7 @@ angular.module('onsComponents')
                 element
                     .find("blockquote").find("p")
                     .addClass("box--padded--highlight");
+
                 // element
                 //     .find("ul").addClass("list--neutral box--cyan--separated-left")
                 //     .find("ul").find("li").addClass("big");
